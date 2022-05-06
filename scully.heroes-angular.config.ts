d336aa098abd37f724d5063131bb30b98012519e
Jonathan Booij <jonathan.booij@rabobank.nl>
2022-05-06 15:37:17 +0200

    router plugin setup

--- scully.heroes-angular.config.ts
+++ scully.heroes-angular.config.ts
@@ -1,13 +1,24 @@
 import { ScullyConfig } from '@scullyio/scully';
-
+import { HandledRoute, registerPlugin } from '@scullyio/scully';
 /** this loads the default render plugin, remove when switching to something else. */
 
-
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "heroes-angular",
+  projectRoot: './src',
+  projectName: 'heroes-angular',
   spsModulePath: 'YOUR OWN MODULE PATH HERE',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
 };
+
+// function heroPlugin(route: string, config = {}): Promise<HandledRoute[]> {
+//   return Promise.resolve([
+//     { route: '/user/1' },
+//     { route: '/user/2' },
+//     { route: '/user/3' },
+//     { route: '/user/4' },
+//     { route: '/user/5' },
+//   ]);
+// }
+
+// const validator = async (conf) => [];
+// registerPlugin('router', 'userIds', heroPlugin, validator);
